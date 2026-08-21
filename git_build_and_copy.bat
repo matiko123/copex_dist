029c713b12c0b30fc19e893c6ff7de5ee9132cd0
unknown <christianstephen025@gmail.com>
2026-08-21 13:05:52 +0300

    m

--- git_build_and_copy.bat
+++ git_build_and_copy.bat
@@ -1,13 +1,28 @@
 @echo off
 
-cd ../frontEnd
+cd /d D:\copexErp\frontEnd
 
-npm run build && start dist
+call npm run build
+if errorlevel 1 (
+    echo Build failed. Aborting copy.
+    pause
+    exit /b 1
+)
+
+@REM robocopy dist D:\copexErp\copex_erp_live_dist\built_files\ /E /PURGE
+robocopy dist D:\copexErp\copex_erp_live_dist\dist\ /E /PURGE
+
+
+
+cd /d D:\copexErp\copex_erp_live_dist
+
+start start git_push.bat
+
+
+@REM start dist
 
 @REM git add .
-
 @REM git commit -m "m"
-
 @REM git push
 
 pause
